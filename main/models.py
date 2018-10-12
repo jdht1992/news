c0fdb9db791251945db1a2c23b8b0e92942029d9
Diego Herrera <jdht1992@gmail.com>
2018-10-12 16:20:03 -0500

    Modificando image

--- main/models.py
+++ main/models.py
@@ -7,16 +7,17 @@ class Source(models.Model):
     def __str__(self):
         return self.name
 
+
 class Article(models.Model):
     author = models.CharField(max_length=500,  null=True)
     title = models.TextField(max_length=500)
     description = models.TextField(max_length=500, null=True)
     url = models.URLField(max_length=200)
-    urltoimage = models.URLField(max_length=200)
-    publishedAt = models.DateField()
+    image = models.URLField(max_length=200, blank=True, null=True)
+    published = models.DateField()
     content = models.TextField(max_length=500, null=True)
     source = models.ForeignKey(Source, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.author
 
