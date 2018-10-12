387c755be929dfd37ad3b280ceea7642b69d5a0d
Diego Herrera <jdht1992@gmail.com>
2018-10-12 16:03:37 -0500

    modificando archivos

--- main/models.py
+++ main/models.py
@@ -4,14 +4,19 @@ from django.db import models
 class Source(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     author = models.CharField(max_length=500,  null=True)
     title = models.TextField(max_length=500)
     description = models.TextField(max_length=500, null=True)
     url = models.URLField(max_length=200)
-    img = models.ImageField(null=True)
+    urltoimage = models.URLField(max_length=200)
     publishedAt = models.DateField()
     content = models.TextField(max_length=500, null=True)
     source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
