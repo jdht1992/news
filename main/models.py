d5e8f41e4c6072ba882d2091925f8ad3253eb252
Diego Herrera <jdht1992@gmail.com>
2018-10-12 15:47:16 -0500

    migrando el proyecto

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=500,  null=True)
+    title = models.TextField(max_length=500)
+    description = models.TextField(max_length=500, null=True)
+    url = models.URLField(max_length=200)
+    img = models.ImageField(null=True)
+    publishedAt = models.DateField()
+    content = models.TextField(max_length=500, null=True)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
